0f27d2f861cf338b6a906421d00b8fd3b6751594
Juan Pablo Eslava <jpesmar@gmail.com>
2018-11-01 10:13:32 +0100

    JP - access: restaurant headers semantic fix #2

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -142,7 +142,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
-  const title = document.createElement('h2');
+  const title = document.createElement('h3');
   title.className = 'restaurant-review-title';
   title.innerHTML = 'Reviews';
   container.appendChild(title);
